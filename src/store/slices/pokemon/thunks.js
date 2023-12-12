d32da85cedd152f25e8bcd1a459c26c1f0a42a15
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2023-12-12 13:25:54 +0100

    Axios

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,3 +1,4 @@
+import { pokemonApi } from '../../../api/pokemonApi';
 import { setPokemons, startLoadingPokemons } from './pokemonSlice';
 
 export const getPokemons = (page = 0) => {
@@ -5,6 +6,15 @@ export const getPokemons = (page = 0) => {
 		dispatch(startLoadingPokemons());
 
 		// TODO: realizar la petición http
-		// dispatch(setPokemons());
+		// const resp = await fetch(
+		// 	`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`,
+		// );
+		// const data = await resp.json();
+
+		const { data } = await pokemonApi.get(
+			`/pokemon?limit=10&offset=${page * 10}`,
+		);
+		// console.log(data);
+		dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
 	};
 };
